ef510ff7a56f38d0e0f3b2cb1259246ba590666a
giorgiomi <giorgio.micaglio@studenti.unitn.it>
2025-01-16 19:15:33 +0100

    update .gitignore

--- src/testFFT.py
+++ src/testFFT.py
@@ -32,7 +32,7 @@
 
 
 
-from scipy.fft import fft, fftfreq
+from scipy.fft import rfft, rfftfreq, fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -40,12 +40,21 @@ import matplotlib.pyplot as plt
 N = 600
 
 # sample spacing
-T = 1.0 / 800.0
+T = 1.0 / 10.0
 x = np.linspace(0.0, N*T, N, endpoint=False)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-yf = fft(y)
-xf = fftfreq(N, T)[:N//2]
+# y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+y = np.sin(x)
+print(len(y), len(x))
 
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
-plt.grid()
+plt.plot(x, y)
 plt.show()
+
+yf = fft(y)
+xf = fftfreq(len(y), d=1)
+
+plt.plot(xf, np.abs(yf))
+# plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+plt.yscale('log')
+plt.show()
+
+print(len(yf), len(xf))
